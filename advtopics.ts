e13d4e4bee216ba754a94eba3712ce6dd40d6b97
JungWhoNam <jungwhonam@gmail.com>
2021-12-16 23:17:54 +0900

    Test Type Union, Assertion

--- advtopics.ts
+++ advtopics.ts
@@ -50,3 +50,60 @@ class Emp implements IEmployee {
 }
 let employee: IEmployee = new Emp(23, "John");
 console.log(employee.getSalary(23));
+
+// Interaction Types
+interface BusinessPartner {
+    name: string;
+    credit: number;
+}
+interface Identity {
+    id: number;
+    name: string;
+}
+interface Contact {
+    email: string;
+    phone: string;
+}
+
+{
+    type Employee = Identity & Contact;
+    let e: Employee = {
+        id: 100,
+        name: "John Doe",
+        email: "JD@email.com",
+        phone: "000-000-0000"
+    }
+}
+
+type Customer = BusinessPartner & Contact;
+let c: Customer = {
+    name: "Tom Doy",
+    credit: 100000,
+    email: "TD@email.com",
+    phone: "000-000-0001"
+}
+
+{
+    type Employee = Identity & BusinessPartner & Contact;
+    let e: Employee = {
+        id: 100,
+        name: "John Doe",
+        email: "JD@email.com",
+        phone: "000-000-0000",
+        credit: 100000
+    }
+}
+
+// type assertions
+function getNetPrice(price:number, discount: number, format: boolean): string | number {
+    let netPrice = price * (1 - discount);
+    return format ? `$${netPrice}` : netPrice;
+}
+{
+    let netPrice = getNetPrice(100, 0.05, true) as string;
+    console.log(netPrice);
+}
+{
+    let netPrice = getNetPrice(100, 0.05, false) as number;
+    console.log(netPrice);
+}
